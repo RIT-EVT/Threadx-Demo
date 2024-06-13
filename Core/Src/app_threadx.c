16e184a4ce90d3f6d3c1d714816622da47cf672c
DiegoLHendrix <119289719+DiegoLHendrix@users.noreply.github.com>
2024-06-13 14:18:48 -0400

    Thread 0 no longer adds to the global stats. Before the thread 1 while loop starts it waits to recieve a semaphore from thread 2 to ensure thread 2 is created before starting so thread 1 does not go through its process twice at the start.

--- Core/Src/app_threadx.c
+++ Core/Src/app_threadx.c
@@ -50,7 +50,7 @@ TX_THREAD thread_2;
 /* Private variables ---------------------------------------------------------*/
 /* USER CODE BEGIN PV */
 TX_QUEUE                queue_0;
-TX_SEMAPHORE            counting_semaphore_0;
+TX_SEMAPHORE            semaphore_0;
 TX_MUTEX                mutex_0;
 TX_EVENT_FLAGS_GROUP    event_flags_0;
 TX_BYTE_POOL            byte_pool_0;
@@ -103,11 +103,14 @@ UINT App_ThreadX_Init(VOID *memory_ptr)
 		  pointer, DEMO_STACK_SIZE,
 		  1, 1, TX_NO_TIME_SLICE, TX_AUTO_START);
 
-  /* Create the message queue shared by threads 1 and 2. */
+  /* Create the message queue shared by all threads. */
   tx_queue_create(&queue_0, "queue 0", TX_1_ULONG, pointer, DEMO_QUEUE_SIZE*sizeof(ULONG));
 
-  /* Create the semaphore used by threads 1 and 2. */
-  tx_semaphore_create(&counting_semaphore_0, "counting semaphore 0", 1);
+  /* Create the semaphore used by all threads. */
+  tx_semaphore_create(&semaphore_0, "counting semaphore 0", 1);
+
+  /* Create the event flags group used by threads. */
+  tx_event_flags_create(&event_flags_0, "event flags 0");
 
   /* USER CODE END App_ThreadX_Init */
   return ret;
@@ -132,12 +135,8 @@ void MX_ThreadX_Init(void)
 }
 
 /* USER CODE BEGIN 1 */
-ULONG shared_count = 0;//Times a random number has been sent to the queue
-ULONG shared_sum = 0;//Sum of random numbers
-//Shared average should match thread0 average
-
-ULONG received_message;
-ULONG queue_status;
+ULONG global_count = 0; //Times a random number has been sent and received
+ULONG global_sum = 0; //Sum of random numbers
 ULONG thread0_count = 0;
 ULONG thread0_sum = 0;
 ULONG thread1_count = 0;
@@ -145,41 +144,39 @@ ULONG thread1_sum = 0;
 ULONG thread2_count = 0;
 ULONG thread2_sum = 0;
 
-ULONG semaphore_status;
-
 void thread_0_entry(ULONG thread_input)
 {
+	ULONG queue_status;
+	ULONG semaphore_status;
     char buffer[300];  // Buffer to hold the formatted string
     srand(time(0));
     ULONG num;
     HAL_UART_Transmit(&huart2, (uint8_t*)"Made Thread 0.\r\n", 17, 100);
+
     /* This thread simply sits in while-forever-sleep loop.  */
     while (1)
     {
-        /* Send message to queue 0. */
-    	//TODO: Add random number instead of count to queue
     	num = rand() % 25 + 1;
-    	//TODO: Thread0 count and average and compare to everything else
-    	//TODO: Global count and average should match thread0 count and average
+
+    	/* Send message to queue 0. */
     	queue_status = tx_queue_send(&queue_0, &num, TX_WAIT_FOREVER);
-        semaphore_status = tx_semaphore_get(&counting_semaphore_0, TX_WAIT_FOREVER);
+
+    	/* Take semaphore when it is released. */
+    	semaphore_status = tx_semaphore_get(&semaphore_0, TX_WAIT_FOREVER);
 
     	if(queue_status == TX_SUCCESS )
     	{
-            shared_count++;
-            shared_sum += num;
             thread0_count++;
             thread0_sum += num;
     	}
 
-
-        //TODO: Should print piece of data
-        if(thread0_count % 10 == 0)
+        if(thread0_count % 20 == 0)
         {
-            // Format the string with thread0_count and sum0 values into the buffer array
+            // Format the string with data into the buffer array
             sprintf(buffer,
-            		"Shared count: %lu\r\n"
-            		"Shared sum: %lu\r\n"
+            		"Global count: %lu\r\n"
+            		"Global sum: %lu\r\n"
+            		"Global average: %lu\r\n"
             		"Thread 0 count: %lu\r\n"
             		"Thread 0 sum: %lu\r\n"
             		"Thread 0 average: %lu\r\n"
@@ -190,7 +187,7 @@ void thread_0_entry(ULONG thread_input)
             		"Thread 2 sum: %lu\r\n"
             		"Thread 2 average: %lu\r\n"
             		"\r\n",
-					shared_count, shared_sum,
+					global_count,  global_sum,  global_sum  / global_count,
 					thread0_count, thread0_sum, thread0_sum / thread0_count,
 					thread1_count, thread1_sum, thread1_sum / thread1_count,
 					thread2_count, thread2_sum, thread2_sum / thread2_count);
@@ -199,7 +196,7 @@ void thread_0_entry(ULONG thread_input)
             HAL_UART_Transmit(&huart2, (uint8_t*)buffer, strlen(buffer), 1000);
         }
 
-        semaphore_status = tx_semaphore_put(&counting_semaphore_0);
+        semaphore_status = tx_semaphore_put(&semaphore_0);
 
         /* Sleep for 100 ticks = 1 second.  */
         tx_thread_sleep(100);
@@ -213,21 +210,29 @@ void thread_0_entry(ULONG thread_input)
 
 void thread_1_entry(ULONG thread_input)
 {
+	ULONG received_message;
+	ULONG queue_status;
+	ULONG semaphore_status;
+	char message_buffer[100]; // Buffer to store the message string
 
 	HAL_UART_Transmit(&huart2, (uint8_t*)"Made Thread 1.\r\n", 17, 100);
-	char message_buffer[100]; // Buffer to store the message string
-    /* This thread simply sits in while-forever-sleep loop.  */
+
+	/* Makes sure that thread 2 is create before starting the while loop. */
+	tx_semaphore_get(&semaphore_0, TX_WAIT_FOREVER);
+
+	/* This thread simply sits in while-forever-sleep loop.  */
     while (1)
     {
         /* Retrieve a message from the queue. */
     	queue_status = tx_queue_receive(&queue_0, &received_message, TX_WAIT_FOREVER);
+
     	/* Get the semaphore with suspension. */
-    	semaphore_status = tx_semaphore_get(&counting_semaphore_0, TX_WAIT_FOREVER);
+    	semaphore_status = tx_semaphore_get(&semaphore_0, TX_WAIT_FOREVER);
 
     	if(queue_status == TX_SUCCESS )
     	{
-            shared_count++;
-            shared_sum += received_message;
+            global_count++;
+            global_sum += received_message;
             thread1_count++;
             thread1_sum += received_message;
     	}
@@ -244,13 +249,12 @@ void thread_1_entry(ULONG thread_input)
 
 
         /* Release the semaphore. */
-        semaphore_status = tx_semaphore_put(&counting_semaphore_0);
+        semaphore_status = tx_semaphore_put(&semaphore_0);
 
         if ( (queue_status != TX_SUCCESS) || (semaphore_status != TX_SUCCESS) )
         {
         	HAL_UART_Transmit(&huart2, (uint8_t*)"Thread 1 Error.\r\n", 17, 100);
         }
-
     }
 }
 
@@ -258,24 +262,27 @@ void thread_2_entry(ULONG thread_input)
 {
 	ULONG received_message;
 	ULONG queue_status;
-
-	HAL_UART_Transmit(&huart2, (uint8_t*)"Made Thread 2.\r\n", 17, 100);
+	ULONG semaphore_status;
 	char message_buffer[100]; // Buffer to store the message string
+
+	HAL_UART_Transmit(&huart2, (uint8_t*)"Made Thread 2.\r\n\r\n", 19, 100);
+
+	/* Signals to thread 1 that thread 2 has been created and the tasks can actually start. */
+	tx_semaphore_put(&semaphore_0);
 
     /* This thread simply sits in while-forever-sleep loop.  */
     while (1)
     {
         queue_status = tx_queue_receive(&queue_0, &received_message, TX_WAIT_FOREVER);
-        semaphore_status = tx_semaphore_get(&counting_semaphore_0, TX_WAIT_FOREVER);
+        semaphore_status = tx_semaphore_get(&semaphore_0, TX_WAIT_FOREVER);
 
     	if(queue_status == TX_SUCCESS )
     	{
-            shared_count++;
-            shared_sum += received_message;
+            global_count++;
+            global_sum += received_message;
             thread2_count++;
             thread2_sum += received_message;
     	}
-
 
         snprintf(message_buffer, sizeof(message_buffer),
         		"Thread 2 received message: %lu\r\n"
@@ -285,13 +292,14 @@ void thread_2_entry(ULONG thread_input)
 
         HAL_UART_Transmit(&huart2, (uint8_t*)message_buffer, strlen(message_buffer), 100);
 
-        semaphore_status = tx_semaphore_put(&counting_semaphore_0);
+        semaphore_status = tx_semaphore_put(&semaphore_0);
 
         if ( (queue_status != TX_SUCCESS) || (semaphore_status != TX_SUCCESS) )
         {
         	HAL_UART_Transmit(&huart2, (uint8_t*)"Thread 2 Error.\r\n", 17, 100);
         }
-
     }
+
+
 }
 /* USER CODE END 1 */
