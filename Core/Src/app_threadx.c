0fe2d6b4dee90b0823fcde257f2594c5944d9518
DiegoLHendrix <119289719+DiegoLHendrix@users.noreply.github.com>
2024-06-15 11:31:52 -0400

    Documentation changes

--- Core/Src/app_threadx.c
+++ Core/Src/app_threadx.c
@@ -1,21 +1,21 @@
 /* USER CODE BEGIN Header */
 /**
-  ******************************************************************************
-  * @file    app_threadx.c
-  * @author  MCD Application Team
-  * @brief   ThreadX applicative file
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2021 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
+ ******************************************************************************
+ * @file    app_threadx.c
+ * @author  MCD Application Team
+ * @brief   ThreadX applicative file
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2021 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
 /* USER CODE END Header */
 
 /* Includes ------------------------------------------------------------------*/
@@ -26,7 +26,6 @@
 #include "usart.h"
 #include "gpio.h"
 #include <stdio.h>  // For sprintf
-#include <time.h>
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -49,20 +48,19 @@ TX_THREAD thread_2;
 
 /* Private variables ---------------------------------------------------------*/
 /* USER CODE BEGIN PV */
-TX_QUEUE                queue_0;
-TX_SEMAPHORE            semaphore_0;
-TX_MUTEX                mutex_0;
-TX_EVENT_FLAGS_GROUP    event_flags_0;
-TX_BYTE_POOL            byte_pool_0;
-TX_BLOCK_POOL           block_pool_0;
-
+TX_QUEUE queue_0;
+TX_SEMAPHORE semaphore_0;
+TX_MUTEX mutex_0;
+TX_EVENT_FLAGS_GROUP event_flags_0;
+TX_BYTE_POOL byte_pool_0;
+TX_BLOCK_POOL block_pool_0;
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
 /* USER CODE BEGIN PFP */
-void    thread_0_entry(ULONG thread_input);
-void    thread_1_entry(ULONG thread_input);
-void    thread_2_entry(ULONG thread_input);
+void thread_0_entry(ULONG thread_input);
+void thread_1_entry(ULONG thread_input);
+void thread_2_entry(ULONG thread_input);
 /* USER CODE END PFP */
 
 /**
@@ -76,43 +74,39 @@ UINT App_ThreadX_Init(VOID *memory_ptr)
   TX_BYTE_POOL *byte_pool = (TX_BYTE_POOL*)memory_ptr;
 
   /* USER CODE BEGIN App_ThreadX_Init */
-  CHAR *pointer = TX_NULL;
+	CHAR *pointer = TX_NULL;
 
-  /* Allocate the stack for thread 0.  */
-  tx_byte_allocate(byte_pool, (VOID **) &pointer, DEMO_STACK_SIZE, TX_NO_WAIT);
+	/* Allocate the stack for thread 0.  */
+	tx_byte_allocate(byte_pool, (VOID**) &pointer, DEMO_STACK_SIZE, TX_NO_WAIT);
 
-  //TODO: look into parameters of tx thread create
-  /* Create the main thread 0.  */
-  tx_thread_create(&thread_0, "thread 0", thread_0_entry, 0,
-		  pointer, DEMO_STACK_SIZE,
-		  1, 1, TX_NO_TIME_SLICE, TX_AUTO_START);
+	//TODO: look into parameters of tx thread create
+	/* Create the main thread 0.  */
+	tx_thread_create(&thread_0, "thread 0", thread_0_entry, 0, pointer,
+			DEMO_STACK_SIZE, 1, 1, TX_NO_TIME_SLICE, TX_AUTO_START);
 
-  /* Allocate the stack for thread 1.  */
-  tx_byte_allocate(byte_pool, (VOID **) &pointer, DEMO_STACK_SIZE, TX_NO_WAIT);
+	/* Allocate the stack for thread 1.  */
+	tx_byte_allocate(byte_pool, (VOID**) &pointer, DEMO_STACK_SIZE, TX_NO_WAIT);
 
-  /* Create the main thread 1.  */
-  tx_thread_create(&thread_1, "thread 1", thread_1_entry, 0,
-  		  pointer, DEMO_STACK_SIZE,
-  		  1, 1, TX_NO_TIME_SLICE, TX_AUTO_START);
+	/* Create the main thread 1.  */
+	tx_thread_create(&thread_1, "thread 1", thread_1_entry, 0, pointer,
+			DEMO_STACK_SIZE, 1, 1, TX_NO_TIME_SLICE, TX_AUTO_START);
 
-  /* Allocate the stack for thread 2.  */
-  tx_byte_allocate(byte_pool, (VOID **) &pointer, DEMO_STACK_SIZE, TX_NO_WAIT);
+	/* Allocate the stack for thread 2.  */
+	tx_byte_allocate(byte_pool, (VOID**) &pointer, DEMO_STACK_SIZE, TX_NO_WAIT);
 
-  /* Create the main thread 2.  */
-  tx_thread_create(&thread_2, "thread 2", thread_2_entry, 0,
-		  pointer, DEMO_STACK_SIZE,
-		  1, 1, TX_NO_TIME_SLICE, TX_AUTO_START);
+	/* Create the main thread 2.  */
+	tx_thread_create(&thread_2, "thread 2", thread_2_entry, 0, pointer,
+			DEMO_STACK_SIZE, 1, 1, TX_NO_TIME_SLICE, TX_AUTO_START);
 
-  /* Create the message queue shared by all threads. */
-  tx_queue_create(&queue_0, "queue 0", TX_1_ULONG, pointer, DEMO_QUEUE_SIZE*sizeof(ULONG));
+	/* Create the message queue shared by all threads. */
+	tx_queue_create(&queue_0, "queue 0", TX_1_ULONG, pointer,
+			DEMO_QUEUE_SIZE*sizeof(ULONG));
 
-  /* Create the semaphore used by all threads. */
-  tx_semaphore_create(&semaphore_0, "counting semaphore 0", 1);
-
-  /* Create the event flags group used by threads. */
-  tx_event_flags_create(&event_flags_0, "event flags 0");
+	/* Create the semaphore used by all threads. */
+	tx_semaphore_create(&semaphore_0, "counting semaphore 0", 1);
 
   /* USER CODE END App_ThreadX_Init */
+
   return ret;
 }
 
@@ -124,7 +118,7 @@ UINT App_ThreadX_Init(VOID *memory_ptr)
 void MX_ThreadX_Init(void)
 {
   /* USER CODE BEGIN  Before_Kernel_Start */
-	HAL_UART_Transmit(&huart2, (uint8_t*)"Kernel starting.\r\n", 20, 100);
+	HAL_UART_Transmit(&huart2, (uint8_t*) "Kernel starting.\r\n", 20, 100);
   /* USER CODE END  Before_Kernel_Start */
 
   tx_kernel_enter();
@@ -144,162 +138,149 @@ ULONG thread1_sum = 0;
 ULONG thread2_count = 0;
 ULONG thread2_sum = 0;
 
-void thread_0_entry(ULONG thread_input)
-{
+void thread_0_entry(ULONG thread_input) {
 	ULONG queue_status;
 	ULONG semaphore_status;
-    char buffer[300];  // Buffer to hold the formatted string
-    srand(time(0));
-    ULONG num;
-    HAL_UART_Transmit(&huart2, (uint8_t*)"Made Thread 0.\r\n", 17, 100);
+	char buffer[300];  // Buffer to hold the formatted string
+	srand(77);
+	ULONG num;
+	HAL_UART_Transmit(&huart2, (uint8_t*) "Made Thread 0.\r\n", 17, 100);
 
-    /* This thread simply sits in while-forever-sleep loop.  */
-    while (1)
-    {
-    	num = rand() % 25 + 1;
+	/* Delay ensures that thread 1 and thread 2 are created before thread 0 adds to the queue. */
+	tx_thread_sleep(TX_TIMER_TICKS_PER_SECOND * 1);
 
-    	/* Send message to queue 0. */
-    	queue_status = tx_queue_send(&queue_0, &num, TX_WAIT_FOREVER);
+	/* This thread simply sits in while-forever-sleep loop.  */
+	while (1) {
+		num = rand() % 25 + 1;
 
-    	/* Take semaphore when it is released. */
-    	semaphore_status = tx_semaphore_get(&semaphore_0, TX_WAIT_FOREVER);
+		/* Send message to queue 0. */
+		queue_status = tx_queue_send(&queue_0, &num, TX_WAIT_FOREVER);
 
-    	if(queue_status == TX_SUCCESS )
-    	{
-            thread0_count++;
-            thread0_sum += num;
-    	}
+		/* Take semaphore when it is released. */
+		semaphore_status = tx_semaphore_get(&semaphore_0, TX_WAIT_FOREVER);
 
-        if(thread0_count % 20 == 0)
-        {
-            // Format the string with data into the buffer array
-            sprintf(buffer,
-            		"Global count: %lu\r\n"
-            		"Global sum: %lu\r\n"
-            		"Global average: %lu\r\n"
-            		"Thread 0 count: %lu\r\n"
-            		"Thread 0 sum: %lu\r\n"
-            		"Thread 0 average: %lu\r\n"
-            		"Thread 1 count: %lu\r\n"
-            		"Thread 1 sum: %lu\r\n"
-            		"Thread 1 average: %lu\r\n"
-            		"Thread 2 count: %lu\r\n"
-            		"Thread 2 sum: %lu\r\n"
-            		"Thread 2 average: %lu\r\n"
-            		"\r\n",
-					global_count,  global_sum,  global_sum  / global_count,
+		if (queue_status == TX_SUCCESS) {
+			thread0_count++;
+			thread0_sum += num;
+		}
+
+		if (thread0_count % 20 == 0) {
+			// Format the string with the thread stats in the buffer array
+			sprintf(buffer, "Global count: %lu\r\n"
+					"Global sum: %lu\r\n"
+					"Global average: %lu\r\n"
+					"Thread 0 count: %lu\r\n"
+					"Thread 0 sum: %lu\r\n"
+					"Thread 0 average: %lu\r\n"
+					"Thread 1 count: %lu\r\n"
+					"Thread 1 sum: %lu\r\n"
+					"Thread 1 average: %lu\r\n"
+					"Thread 2 count: %lu\r\n"
+					"Thread 2 sum: %lu\r\n"
+					"Thread 2 average: %lu\r\n"
+					"\r\n", global_count, global_sum, global_sum / global_count,
 					thread0_count, thread0_sum, thread0_sum / thread0_count,
 					thread1_count, thread1_sum, thread1_sum / thread1_count,
 					thread2_count, thread2_sum, thread2_sum / thread2_count);
 
-            // Transmit the formatted string
-            HAL_UART_Transmit(&huart2, (uint8_t*)buffer, strlen(buffer), 1000);
-        }
+			// Transmit the formatted string
+			HAL_UART_Transmit(&huart2, (uint8_t*) buffer, strlen(buffer), 1000);
+		}
 
-        semaphore_status = tx_semaphore_put(&semaphore_0);
+		semaphore_status = tx_semaphore_put(&semaphore_0);
 
-        /* Sleep for 100 ticks = 1 second.  */
-        tx_thread_sleep(100);
+		if ((queue_status != TX_SUCCESS) || (semaphore_status != TX_SUCCESS)) {
+			HAL_UART_Transmit(&huart2, (uint8_t*) "Thread 0 Error.\r\n", 17,
+					100);
+		}
 
-        if ( (queue_status != TX_SUCCESS) || (semaphore_status != TX_SUCCESS) )
-        {
-            HAL_UART_Transmit(&huart2, (uint8_t*)"Thread 0 Error.\r\n", 17, 100);
-        }
-    }
+		/* Sleep for 1 second in ticks.  */
+		tx_thread_sleep(TX_TIMER_TICKS_PER_SECOND * 1);
+	}
 }
 
-void thread_1_entry(ULONG thread_input)
-{
+void thread_1_entry(ULONG thread_input) {
 	ULONG received_message;
 	ULONG queue_status;
 	ULONG semaphore_status;
 	char message_buffer[100]; // Buffer to store the message string
 
-	HAL_UART_Transmit(&huart2, (uint8_t*)"Made Thread 1.\r\n", 17, 100);
-
-	/* Makes sure that thread 2 is create before starting the while loop. */
-	tx_semaphore_get(&semaphore_0, TX_WAIT_FOREVER);
+	HAL_UART_Transmit(&huart2, (uint8_t*) "Made Thread 1.\r\n", 17, 100);
 
 	/* This thread simply sits in while-forever-sleep loop.  */
-    while (1)
-    {
-        /* Retrieve a message from the queue. */
-    	queue_status = tx_queue_receive(&queue_0, &received_message, TX_WAIT_FOREVER);
+	while (1) {
+		/* Retrieve a message from the queue. */
+		queue_status = tx_queue_receive(&queue_0, &received_message,
+				TX_WAIT_FOREVER);
 
-    	/* Get the semaphore with suspension. */
-    	semaphore_status = tx_semaphore_get(&semaphore_0, TX_WAIT_FOREVER);
+		/* Get the semaphore with suspension. */
+		semaphore_status = tx_semaphore_get(&semaphore_0, TX_WAIT_FOREVER);
 
-    	if(queue_status == TX_SUCCESS )
-    	{
-            global_count++;
-            global_sum += received_message;
-            thread1_count++;
-            thread1_sum += received_message;
-    	}
+		if (queue_status == TX_SUCCESS) {
+			global_count++;
+			global_sum += received_message;
+			thread1_count++;
+			thread1_sum += received_message;
+		}
 
+		snprintf(message_buffer, sizeof(message_buffer),
+				"Thread 1 received message: %lu\r\n"
+						"Thread 1 count: %lu\r\n"
+						"Thread 1 sum: %lu\r\n"
+						"\r\n", received_message, thread1_count, thread1_sum);
 
-        snprintf(message_buffer, sizeof(message_buffer),
-        		"Thread 1 received message: %lu\r\n"
-        		"Thread 1 count: %lu\r\n"
-        		"Thread 1 sum: %lu\r\n"
-        		"\r\n",
-        		received_message, thread1_count, thread1_sum);
+		HAL_UART_Transmit(&huart2, (uint8_t*) message_buffer,
+				strlen(message_buffer), 100);
 
-        HAL_UART_Transmit(&huart2, (uint8_t*)message_buffer, strlen(message_buffer), 100);
+		/* Release the semaphore. */
+		semaphore_status = tx_semaphore_put(&semaphore_0);
 
+		if ((queue_status != TX_SUCCESS) || (semaphore_status != TX_SUCCESS)) {
+			HAL_UART_Transmit(&huart2, (uint8_t*) "Thread 1 Error.\r\n", 17,
+					100);
+		}
 
-        /* Release the semaphore. */
-        semaphore_status = tx_semaphore_put(&semaphore_0);
-
-        if ( (queue_status != TX_SUCCESS) || (semaphore_status != TX_SUCCESS) )
-        {
-        	HAL_UART_Transmit(&huart2, (uint8_t*)"Thread 1 Error.\r\n", 17, 100);
-        }
-    }
+		tx_thread_sleep(TX_TIMER_TICKS_PER_SECOND * 1);
+	}
 }
 
-void thread_2_entry(ULONG thread_input)
-{
+void thread_2_entry(ULONG thread_input) {
 	ULONG received_message;
 	ULONG queue_status;
 	ULONG semaphore_status;
 	char message_buffer[100]; // Buffer to store the message string
 
-	HAL_UART_Transmit(&huart2, (uint8_t*)"Made Thread 2.\r\n\r\n", 19, 100);
+	HAL_UART_Transmit(&huart2, (uint8_t*) "Made Thread 2.\r\n\r\n", 19, 100);
 
-	/* Signals to thread 1 that thread 2 has been created and the tasks can actually start. */
-	tx_semaphore_put(&semaphore_0);
+	/* This thread simply sits in while-forever-sleep loop.  */
+	while (1) {
+		queue_status = tx_queue_receive(&queue_0, &received_message,
+				TX_WAIT_FOREVER);
+		semaphore_status = tx_semaphore_get(&semaphore_0, TX_WAIT_FOREVER);
 
-    /* This thread simply sits in while-forever-sleep loop.  */
-    while (1)
-    {
-        queue_status = tx_queue_receive(&queue_0, &received_message, TX_WAIT_FOREVER);
-        semaphore_status = tx_semaphore_get(&semaphore_0, TX_WAIT_FOREVER);
+		if (queue_status == TX_SUCCESS) {
+			global_count++;
+			global_sum += received_message;
+			thread2_count++;
+			thread2_sum += received_message;
+		}
 
-    	if(queue_status == TX_SUCCESS )
-    	{
-            global_count++;
-            global_sum += received_message;
-            thread2_count++;
-            thread2_sum += received_message;
-    	}
+		snprintf(message_buffer, sizeof(message_buffer),
+				"Thread 2 received message: %lu\r\n"
+						"Thread 2 count: %lu\r\nThread 2 sum: %lu\r\n"
+						"\r\n", received_message, thread2_count, thread2_sum);
 
-        snprintf(message_buffer, sizeof(message_buffer),
-        		"Thread 2 received message: %lu\r\n"
-        		"Thread 2 count: %lu\r\nThread 2 sum: %lu\r\n"
-        		"\r\n",
-        		received_message, thread2_count, thread2_sum);
+		HAL_UART_Transmit(&huart2, (uint8_t*) message_buffer,
+				strlen(message_buffer), 100);
 
-        HAL_UART_Transmit(&huart2, (uint8_t*)message_buffer, strlen(message_buffer), 100);
+		semaphore_status = tx_semaphore_put(&semaphore_0);
 
-        semaphore_status = tx_semaphore_put(&semaphore_0);
+		if ((queue_status != TX_SUCCESS) || (semaphore_status != TX_SUCCESS)) {
+			HAL_UART_Transmit(&huart2, (uint8_t*) "Thread 2 Error.\r\n", 17,
+					100);
+		}
 
-        if ( (queue_status != TX_SUCCESS) || (semaphore_status != TX_SUCCESS) )
-        {
-        	HAL_UART_Transmit(&huart2, (uint8_t*)"Thread 2 Error.\r\n", 17, 100);
-        }
-    }
-
-
+		tx_thread_sleep(TX_TIMER_TICKS_PER_SECOND * 1);
+	}
 }
 /* USER CODE END 1 */
--- README.md
+++ README.md
@@ -3,37 +3,38 @@
 - RTOS code is in `ThreadX-Demo/Core/Src/app_ThreadX.c`
 - Using 3 threads with normal priority:
 
-The point of this demo is to have an example of how we can create and use threads in ThreadX.
+The point of this demo is to see if threadx is able to run on th EVT platform and to have something to compare the behavior to when we add this to EVT-Core.
 
 ## Thread 0:
- 1. Generates a random number and, using tx_queue_send, adds it to a queue.
- 2. Waits to be able to take a counting semaphore(When the code is first flashed the semaphore starts in the released state so thread 0 should always get it first).
- 3. If the random number is successfully added to the queue a thread 0 specific count increments by 1 and the random number is added to a thread 0 sum.
- 4. Releases a counting semaphore.
- 5. tx_thread_sleep(100) makes the thread sleep for 100 ticks or 1 second.
- 6. Every time the count is incremented 10 times it calculates the average of all thread data and then prints the average along with all other stats in the RTOS.
+ 1. After intializing all the variables there is a tx_thread_sleep that is used stop the thread from adding to the queue so thread 1 and 2 stay in sync. 
+ 2. Generates a random number and, using tx_queue_send, adds it to a queue.
+ 3. Waits to be able to take the semaphore(When the code is first flashed the semaphore starts in the released state so thread 0 should always get it first).
+ 4. If the random number is successfully added to the queue a thread 0 specific count increments by 1 and the random number is added to a thread 0 sum.
+ 5. Releases the semaphore.
+ 6. tx_thread_sleep(100) makes the thread sleep for 100 ticks or 1 second.
+ 7. Every time the count is incremented 20 times it calculates the average of all thread data and then prints the average along with all other stats in the RTOS.
  
  ## Thread 1/2:
  1. After thread0 releases the semaphore for the first time thread 1 and thread 2 are created.
- 2. Thread 1 waits to receive a semaphore signal from thread 2 to ensure thread 1 does not start the process before thread 2 is created. Otherwise, thread 1 would go through its process twice before thread 2 goes once.
- 3. Inside thread 1 tx_queue_receive waits forever until it has received a message from the queue.
- 4. Wait until the semaphore is released.
- 5. If tx_queue_receive has received the random number, thread 1 increments a global and thread 1 count and adds the random number to a global and thread 1 sum.
- 6. Print out the random number it received, a thread 1 count, and a thread 1 sum over UART.
- 7. After thread 1 is done printing, it releases the semaphore 
- 8. Thread 0 goes through the message-sending process again.
- 9. After thread0 releases the semaphore a second time thread 2 goes through steps 2-6 with its variables.
+ 2. Inside thread 1 tx_queue_receive waits forever until it has received a message from the queue.
+ 3. Wait until the semaphore is released.
+ 4. If tx_queue_receive has received the random number, thread 1 increments a global and thread 1 count and adds the random number to a global and thread 1 sum.
+ 5. Print out the random number it received, a thread 1 count, and a thread 1 sum over UART.
+ 6. After thread 1 is done printing, it releases the semaphore 
+ 7. Thread 0 goes through the message-sending process again.
+ 8. After thread0 releases the semaphore a second time thread 2 goes through steps 2-6 with its variables.
  
  ## Global and Thread variables
  - The global and thread variables are used to show how threads communicate using context switching.
  
- ## IOC file
- 1. Download the X-XUBE-AZURE-F4 software package in cubemx.
-	- Scroll down to the package and install version 1.1.0 
+ ## STM32CubeIDE
+ 1. Open the .ioc file.
+ 2. In the middlware category download the X-CUBE-AZURE-F4 software package.
+	- Scroll down to the package and install version 1.1.0.
 	- Open the drop down menu and find RTOS ThreadX.
 	- Open the RTOS ThreadX drown and then another Threadx dropdown menu and check of the 'Core' option. 
 	- Click ok then in the middleware and software packages category of cubemx find X-XUBE-AZURE-F4 and enable it by checking the RTOS ThreadX box.
- 2. Change memory pool size to be 65536.
- 3. In the system core category 
+	- Under configuration in the AzureRTOS Application tab change memory pool size to be 65536. This ensures we have enough allocated memory space.
+ 3. In the system core category.
 	- open RCC and change HSE and LSE to be Crystal/Ceramic Resonator.
 	- Open SYS and change the timebase source to be TIM6.
